318bf9c7613dd4d2b2d1e5a3844d92eeb0ed5ba8
ALI ZULFIQAR <alimalikali1928@gmail.com>
2024-11-20 22:26:52 +0500

    Solved build log errors and types check to false

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { ReactNode } from "react";
-import {
-  LiveblocksProvider, RoomProvider, ClientSideSuspense,
-} from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
 import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
 import { useUser } from "@clerk/nextjs";
+import {
+  ClientSideSuspense,
+  LiveblocksProvider
+} from "@liveblocks/react/suspense";
+import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode}) => {
 
@@ -30,7 +31,6 @@ const Provider = ({ children }: { children: ReactNode}) => {
 
         return roomUsers;
       }}
-    // publicApiKey={"pk_prod_y1Z2FAlt9U4qFAs-vez43RHkpE1LbY0YggaQM95DqmkB4Tyg51YN7osogOYhV7JS"}
     >
       <ClientSideSuspense fallback={<Loader />}>
         {children}
